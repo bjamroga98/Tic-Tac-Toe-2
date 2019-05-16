db1a50cbb38941b5372a99d5e3531d834289d876
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-05-16 09:12:03 +0200

    commit

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp8
+namespace Cw_03_P2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+            char[,] plansza = new char[3, 3] {
+ { '1', '2', '3' },
+ { '4', '5', '6' },
+ { '7', '8', '9' }
+ };
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
+            // Petla na kolejne ruchy graczy
+            bool koniecGry = false;
+            while (!koniecGry)
+            {
+                Console.Clear();
+                RysujPlansze(plansza);
+                Console.ReadKey();
+                // ...
+            }
+        }
+        ////////////////////////////////////////////////
+
+        static void RysujPlansze(char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+
+            for (int i = 0; i < wysokosc; ++i)
+            {
+                for (int j = 0; j < szerokosc; ++j)
+                    Console.Write(plansza[i, j]);
+                Console.WriteLine();
+            }
         }
     }
 }
